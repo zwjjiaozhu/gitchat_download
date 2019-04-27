ce5d5d447e50462ed2d66c4ba84822eed96a850e
教主 <34623422+jz46@users.noreply.github.com>
2019-04-27 10:56:02 +0800

    Update gitchat_download.py

--- py/gitchat_download.py
+++ py/gitchat_download.py
@@ -17,7 +17,6 @@ headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
     'Connection': 'keep-alive'
 }
-# cookies = dict(customerToken='f782f8a0-6e43-11e8-9722-a7ceb2ddb814', customerId='5b1fc99a26021e4b80b09317')
 
 
 class TestThread:
